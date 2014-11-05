9a1bfd3700a9c5319d37bf7a52ce2bca18687504
leoyakubov <leo.yakubov@gmail.com>
2014-11-05 11:19:45 +0200

    Fixed MainAspect

--- src/main/java/io/leonid/springcash/aop/MainAspect.java
+++ src/main/java/io/leonid/springcash/aop/MainAspect.java
@@ -15,25 +15,13 @@ import org.slf4j.LoggerFactory;
 public class MainAspect {
     private static final Logger logger = LoggerFactory.getLogger(MainAspect.class);
 
-    /*@Before("allServiceMethodsPointcut()")
+    @Before("allServiceMethodsPointcut()")
     public void allServiceMethodsAdvice(JoinPoint joinPoint){
         logger.info("Service method called: {}", joinPoint.toString());
-    }*/
-
-    @Before(value = "allControllerMethodsPointcut()")
-    public void allControllerMethodsAdvice(JoinPoint joinPoint){
-        logger.info("==========================================");
-        logger.info("Controller method called: {}", joinPoint.toString());
     }
 
-    /*//Pointcut to execute on all the methods of service classes
+    //Pointcut to execute on all the methods of service classes
     @Pointcut("within(io.leonid.springcash.service.impl.*)")
     public void allServiceMethodsPointcut(){
-    }*/
-
-    //Pointcut to execute on all the methods of service classes
-    @Pointcut("execution(* io.leonid.springcash.web.HomeController.*(..))")
-    //@Pointcut(value = "execution(* io.leonid.springcash.web.user.UserController.*(..))")
-    public void allControllerMethodsPointcut(){
     }
 }
